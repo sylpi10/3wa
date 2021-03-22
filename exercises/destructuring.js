448374ef79125f6d48712905a46f61bb44f4e7a5
sylpi <syl.pillet@hotmail.fr>
2021-03-22 17:08:22 +0100

    destructuring exos

--- exercises/destructuring.js
+++ exercises/destructuring.js
@@ -6,7 +6,41 @@ let student = {
 
 // TODO ...
 
-[firstname, notes, average] = [student.name, student.notes, student.average];
+// [firstname, notes, average] = [student.name, student.notes, student.average];
+const { name, notes, average } = student;
+
 
 // constantes
-console.log(firstname, notes, average);
+console.log(name, notes, average);
+student.name = "Mike";
+notes.push(20);
+console.log(notes);
+console.log(student);
+
+const students = [
+	{
+		name: "Alan",
+		family: {
+			mother: "Isa",
+			father: "Philippe",
+			sister: "Sylvie",
+		},
+		age: 35,
+	},
+	{
+		name: "Bernard",
+		family: {
+			mother: "Patricia",
+			father: "Cécile",
+			sister: "Annie",
+		},
+		age: 55,
+	},
+];
+
+// for of exo
+console.log("\n ------- ");
+
+for (const student of students) {
+    console.log(`name: ${student.name}, sister: ${student.family.sister}\n `);
+}
