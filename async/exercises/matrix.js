b1ccd95e168f87fbaa062620a9756ed63c4878d2
sylpi <syl.pillet@hotmail.fr>
2021-03-24 16:47:31 +0100

    matrix v1

--- async/exercises/matrix.js
+++ async/exercises/matrix.js
@@ -19,18 +19,16 @@ getJsonFromFile('./data/matrix.json')
  .then(({matrix}) => {
      // filter nums
      for (const array of matrix) {
-        //  console.log(array);
-        //  array.filter(a => console.log(typeof(a)));
+      
         const newArr = [];
          const nums = array.filter(m => typeof(m) === 'number');
          let average = array.filter(m => typeof(m) !== 'number');
          average = parseFloat(nums.reduce((total, curr) => total + curr)/ nums.length);
-        //  console.log(average);
-        //  console.log(nums);
+      
+        // generate new tabs where NONE = average
          newArr.push(average, nums);
          console.log(newArr);
      }
 
-     // generate new tabs where NONE = average
 
  })
