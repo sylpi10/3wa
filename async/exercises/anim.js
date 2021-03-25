f821c2bce90852a5d418f4f038aea07a73bd43ad
sylpi <syl.pillet@hotmail.fr>
2021-03-25 10:21:15 +0100

    anim v1

--- async/exercises/anim.js
+++ async/exercises/anim.js
@@ -2,10 +2,11 @@ const anim = (word) => {
 
     return new Promise((resolve, reject)=> {
         const rand = Math.floor(Math.random() * Math.floor(26));
+        const randRes = Math.floor(Math.random() * Math.floor(10));
         const splitted = word.split('');
         const letter = splitted[rand];
 
-        resolve((console.log(`${letter}${rand}`)));
+        resolve((console.log(`${letter}${randRes}`)));
     });
 } 
 
